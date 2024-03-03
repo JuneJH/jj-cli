f20916a5e8a899a530937930da1efa02f01b92c6
juhong <15310487388@163.com>
2024-03-03 22:37:10 +0800

    fix(*): 修改函数引用错误

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -3,7 +3,7 @@ const importLocal = require("import-local");
 const {log} = require("@jj-cli/tools");
 
 if(importLocal(__filename)){
-    log("cli","正在使用jj-cli本地版本");
+    log.info("cli","正在使用jj-cli本地版本");
 }else{
     require("../lib")(process.argv.slice(2));
 }
